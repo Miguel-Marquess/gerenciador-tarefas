bd4cb78400310076a8d798bc2de84cf1e922902d
Miguel-Marquess <mluiz4784@gmail.com>
2026-05-01 01:47:10 -0300

    feat: novas funcoes de validar id antes de fazer requisicoes diretas ao banco, retornando erro 400 se for invalido. E funcao de verificar se um item existe, retornando erro 404 se nao existir.

--- schema/tarefa.py
+++ schema/tarefa.py
@@ -1,3 +1,8 @@
+from bson import ObjectId
+from bson.errors import InvalidId
+from fastapi import HTTPException
+
+
 def tarefaSerializada(tarefa):
     return {
         "id": str(tarefa['_id']),
@@ -5,10 +10,21 @@ def tarefaSerializada(tarefa):
         "descricao": tarefa['descricao'],
         "prioridade": tarefa['prioridade'],
         "concluida": tarefa['concluida'],
-        "data_criacao": tarefa['data_criacao'],
-        "prazo": tarefa['prazo']
+        "data_criacao": str(tarefa['data_criacao']),
+        "prazo": str(tarefa['prazo'])
     }
 
 def lista_tarefas_serializadas(lista_tarefas):
     lista_tarefas = [tarefaSerializada(tarefa) for tarefa in lista_tarefas]
     return lista_tarefas
+
+def validar_id(id):
+    try:
+        return ObjectId(id)
+    except InvalidId:
+        raise HTTPException(status_code=400, detail="ID inserido é inválido.")
+    
+def retorna_se_existir(item):
+    if item:
+        return item
+    raise HTTPException(status_code=404, detail="A Tarefa nao existe.")
